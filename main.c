16dcb55323caf0ca69474c66aeb40fb13b62363e
Rant <yamen74653@gmail.com>
2026-05-16 20:22:34 +0300

    A C console application that calculates the areas of various geometric shapes (Square, Rectangle, Triangle, Circle, and Trapezoid) interactively using loops and math functions.

--- main.c
+++ main.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
     int choice;
-    float num1,num2,area;
-    printf("1-Square\n2-Rectangular\n3-Triangle\n");
-    printf("What area do you want to enter ? ");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1 :
-            printf("Insert one side of the square : ");
-            scanf("%f",&num1);
-            area = num1*num1;
-            printf("The area of the square is %.2f\n",area);
-            break;
-        case 2 :
-            printf("Enter the length of the rectangle : ");
-            scanf("%f",&num1);
-            printf("Enter the width of the rectangle : ");
-            scanf("%f",&num2);
-            area = num1*num2;
-            printf("The area of the rectangle is %.2f\n",area);
-            break;
-        case 3 :
-            printf("Enter the base of the triangle : ");
-            scanf("%f",&num1);
-            printf("Enter the height of the triangle : ");
-            scanf("%f",&num2);
-            area = 0.5*num1*num2;
-            printf("The area of the triangle is %.2f\n",area);
-            break;
-        default :
-            printf("Invalid number!\n");
-            printf("Try Again.\n");
+    float num1,num2,num3,area;
+    char count = 'y' ;
+    while(count != 'n' && count != 'N'){
+        printf("1-Square\n2-Rectangle\n3-Triangle\n4-Circle\n5-Trapezoid\n");
+        printf("What area do you want to enter ? ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1 :
+                printf("Insert one side of the square : ");
+                scanf("%f",&num1);
+                area = num1*num1;
+                printf("\nThe area of the square is %.2f\n",area);
+                break;
+            case 2 :
+                printf("Enter the length of the rectangle : ");
+                scanf("%f",&num1);
+                printf("Enter the width of the rectangle : ");
+                scanf("%f",&num2);
+                area = num1*num2;
+                printf("\nThe area of the rectangle is %.2f\n",area);
+                break;
+            case 3 :
+                printf("Enter the base of the triangle : ");
+                scanf("%f",&num1);
+                printf("Enter the height of the triangle : ");
+                scanf("%f",&num2);
+                area = 0.5*num1*num2;
+                printf("\nThe area of the triangle is %.2f\n",area);
+                break;
+            case 4 :
+                printf("What is the radius of the circle? : ");
+                scanf("%f",&num1);
+                area = (22/7.0) * pow(num1,2);
+                printf("\nThe area of the circle is %.2f\n",area);
+                break;
+            case 5 :
+                printf("Enter the two parallel bases (separated by a space): ");
+                scanf("%f %f",&num1,&num2);
+                printf("I need you to give me the height of the trapezoid : ");
+                scanf("%f",&num3);
+                area = 0.5*(num1+num2)*num3;
+                printf("\nThe area of the trapezoid is %.2f\n",area);
+                break;
+            default :
+                printf("Invalid number!\n");
+                printf("Try Again.\n");
+                break;
+        }
+        printf("\nDo you want to continue?\nYou can enter any value, but the moment you enter N or n, the application will terminate.\n : ");
+        scanf(" %c",&count);
     }
+
     return 0;
 }
